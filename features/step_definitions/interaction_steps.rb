970d0ed58973001fbe12c2e8609be496277b3160
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-05-06 21:53:27 +0200

    Added comment_article feature implementetd steps

--- features/step_definitions/interaction_steps.rb
+++ features/step_definitions/interaction_steps.rb
@@ -1,4 +1,13 @@
 Given("I visit the site") do
     visit root_path
   end
+
+  Given("I click {string} button") do |button|
+    click_link_or_button button
+  end
+  
+  Given("I fill in {string} with {string}") do |field, text|
+    fill_in field, with: text
+  end
+  
   
